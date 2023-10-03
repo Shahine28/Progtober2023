fa7a8a39c8a30d84892f53f43467e70859c46826
Shahine <shahine.benthanane@edu.devinci.fr>
2023-10-04 00:09:17 +0200

    Jour 2 Terminé et Jour 2 presque fini

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] float Damage;
+    [SerializeField] float Speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.GetComponent<Rigidbody2D>().velocity = transform.up * Speed;
+        /*Vector3 direction = GameObject.FindGameObjectWithTag("Player").transform.position - transform.position;
+        float rot = Mathf.Atan2(-direction.y, -direction.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0,0,rot+90); */
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            /*collision.gameObject.GetComponent<Health>().takeDamage(Damage);*/
+        }
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/SpiderBehavior.cs
+++ Assets/Scripts/SpiderBehavior.cs
@@ -5,36 +5,98 @@ using static UnityEngine.GraphicsBuffer;
 
 public class SpiderBehavior : MonoBehaviour
 {
-    [SerializeField] float ZoneDegat;
-    [SerializeField] float ZoneSuivie;
-    [SerializeField] bool isSetUp;
-    [SerializeField] bool seePlayer;
+    [Header("Player Detection")]
+    public float ZoneDegat;
+    public float ZoneSuivie;
+    [SerializeField] private bool isSetUp;
+    public bool seePlayer;
+
+    [Header("Spider Weapon")]
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] Transform firePoint;
+    [SerializeField] float fireForce;
+    [SerializeField] float cadence;
+    float cadenceTmp;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cadenceTmp = cadence;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (seePlayer)
+        {
+            if (gameObject.GetComponent<Pathfinding.AIDestinationSetter>() != null)
+            {
+                gameObject.GetComponent<Pathfinding.AIDestinationSetter>().enabled = true;
+            }
+        }
+        else if (!seePlayer) 
+        {
+            if (gameObject.GetComponent<Pathfinding.AIDestinationSetter>() != null)
+            {
+                gameObject.GetComponent<Pathfinding.AIDestinationSetter>().enabled = false;
+            } 
+        }
+            
         playerDetection();
+        
     }
 
     private void playerDetection()
     {
-        Collider2D[] zoneInterraction = Physics2D.OverlapCircleAll(transform.position, ZoneDegat);
-        foreach (Collider2D collision in zoneInterraction)
+        Collider2D[] zoneDegat = Physics2D.OverlapCircleAll(transform.position, ZoneDegat);
+        if (zoneDegat.Length > 0)
         {
-            if (collision.gameObject.tag == "Player")
+            foreach (Collider2D collision in zoneDegat)
             {
-                isSetUp = true;
-                Vector2 direction = new Vector2(collision.transform.position.x - transform.position.x, collision.transform.position.y - transform.position.y);
-                transform.up = direction;
+                if (collision.gameObject.tag == "Player")
+                {
+                    isSetUp = true;
+                    Vector2 direction = new Vector2(collision.transform.position.x - transform.position.x, collision.transform.position.y - transform.position.y);
+                    transform.up = direction;
+                    cadence -= Time.deltaTime;
 
+                    if (cadence <= 0)
+                    {
+                        Shoot();
+                        cadence = cadenceTmp;
+                    }
+
+                }
+                else isSetUp = false;
             }
-            else isSetUp = false;
         }
+        else
+        {
+            cadence = cadenceTmp;
+            isSetUp = false;
+        }
+
+        Collider2D[] zoneSuivie = Physics2D.OverlapCircleAll(transform.position, ZoneSuivie);
+        if (zoneSuivie.Length > 0)
+        {
+            foreach (Collider2D collision in zoneSuivie)
+            {
+                if (collision.gameObject.tag == "Player")
+                {
+                    seePlayer = true;
+                }
+                else seePlayer = false;
+            }
+        }
+        else seePlayer = false;
+
+
+
+    }
+
+    void Shoot()
+    {
+        GameObject Bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Force);
     }
 
     private void OnDrawGizmos()
@@ -43,6 +105,7 @@ public class SpiderBehavior : MonoBehaviour
         Gizmos.DrawWireSphere(transform.position, ZoneDegat);
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, ZoneSuivie);
-    }
-
+    } 
 }
+
+
