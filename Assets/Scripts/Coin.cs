4a028418595d9c1cb62e18cb8fa6d4efa3d78c52
Shahine <shahine.benthanane@edu.devinci.fr>
2023-10-09 12:24:27 +0200

    Bounce

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -40,6 +40,7 @@ public class Coin : MonoBehaviour
     }*/
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        
         if (collision.gameObject.tag == "Coin1")
         {  
             Destroy(collision.gameObject);
