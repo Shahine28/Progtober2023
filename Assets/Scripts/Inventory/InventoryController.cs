f50e264859aac335620088e200f3e5d6eeef6249
Shahine <shahine.benthanane@edu.devinci.fr>
2023-10-12 11:52:33 +0200

    Inventaire Début

--- Assets/Scripts/Inventory/InventoryController.cs
+++ Assets/Scripts/Inventory/InventoryController.cs
@@ -8,5 +8,49 @@ using UnityEngine.InputSystem;
 public class InventoryController : MonoBehaviour
 {
     [SerializeField] private UIInventoryPage inventoryUI;
+    [SerializeField] private InputGame _playerInput;
+
+    [SerializeField] private int inventorySize = 54;
+    private void Awake()
+    {
+        _playerInput = new InputGame();
+
+    }
+
+    private void Start()
+    {
+        inventoryUI.InitializeInventoryUI(inventorySize);
+    }
+    public void inventory(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            /*Debug.Log("J'appuie");*/
+            if (!inventoryUI.isActiveAndEnabled) 
+            {
+                inventoryUI.Show();
+            }
+            else
+            {
+                inventoryUI.Hide();
+            }
+
+        }
+        else if (context.canceled)
+        {
+            /*Debug.Log("Je relache");*/
+        }
+    }
+
+    private void OnEnable()
+    {
+        _playerInput.Player.Inventory.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerInput.Player.Inventory.Disable();
+
+    }
 
 }
