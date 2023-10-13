ed931fb509b468106e6576a742c7309a3ed38508
Shahine <shahine.benthanane@edu.devinci.fr>
2023-10-13 18:02:02 +0200

    Inventaire suite
    Partie 8

--- Assets/Scripts/UI/UIInventoryItem.cs
+++ Assets/Scripts/UI/UIInventoryItem.cs
@@ -1,17 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Runtime.CompilerServices;
+using System;
+using UnityEngine.EventSystems;
 
 public class UIInventoryItem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Image itemImage;
+    [SerializeField] private TextMeshProUGUI quantityTxt;
+    [SerializeField] private Image borderImage;
+
+    public event Action<UIInventoryItem> OnItemClicked, OnItemDropped, OnItemBeginDrag, OnItemEndDrag, OnRightMouseBtnClick;
+    private bool empty = true;
+
+    private void Awake()
     {
-        
+        ResetData();
+        Deselect();
+    }
+
+    public void ResetData()
+    {
+        this.itemImage.gameObject.SetActive(false);
+        this.empty = true;
+    }
+    public void Deselect()
+    {
+        borderImage.enabled = false; 
+    }
+
+    public void Select()
+    {
+        borderImage.enabled = true;
+    }
+    
+
+    public void SetData(Sprite sprite, int quantity)
+    {
+        this.itemImage.gameObject.SetActive(true);
+        this.itemImage.sprite = sprite;
+        this.quantityTxt.text = quantity + "";
+        this.empty = false;
+    }
+
+    public void OnBeginDrag()
+    {
+        if (empty) return;
+        OnItemBeginDrag?.Invoke(this);
+    }
+    public void OnDrop()
+    {
+        OnItemDropped?.Invoke(this);
+    }
+
+    public void OnEndDrag()
+    {
+        OnItemEndDrag?.Invoke(this);
+    }
+
+    public void OnPointerClick(BaseEventData data)
+    {
+        if (empty) return;
+        PointerEventData pointerData = (PointerEventData)data;
+        if (pointerData.button == PointerEventData.InputButton.Right)
+        {
+            OnRightMouseBtnClick?.Invoke(this);
+        }
+        else
+        {
+            OnItemClicked?.Invoke(this);
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
         
     }
--- Assets/Scripts/UI/UIInventoryPage.cs
+++ Assets/Scripts/UI/UIInventoryPage.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class UIInventoryPage : MonoBehaviour
@@ -9,6 +11,8 @@ public class UIInventoryPage : MonoBehaviour
     [SerializeField] private RectTransform contentPanel;
     private List<UIInventoryItem> listOfUIItems = new List<UIInventoryItem>();
 
+    [SerializeField] List<LootFortune> listOfObjects  = new List<LootFortune>();
+
     public void InitializeInventoryUI(int inventorySize)
     {
         for(int i = 0; i < inventorySize; i++)
@@ -16,12 +20,46 @@ public class UIInventoryPage : MonoBehaviour
             UIInventoryItem uiItem = Instantiate(itemPrefab,Vector3.zero,Quaternion.identity);
             uiItem.transform.SetParent(contentPanel);
             listOfUIItems.Add(uiItem);
+            uiItem.OnItemClicked += HandleItemSelection;
+            uiItem.OnItemBeginDrag += HandleBeginDrag;
+            uiItem.OnItemEndDrag += HandleEndDrag;
+            uiItem.OnItemDropped += HandleSwap;
+            uiItem.OnRightMouseBtnClick += HandleShowItemActions;
         }
+    }
+
+    private void HandleShowItemActions(UIInventoryItem item)
+    {
+        
+    }
+
+    private void HandleSwap(UIInventoryItem item)
+    {
+        
+    }
+
+    private void HandleEndDrag(UIInventoryItem item)
+    {
+        
+    }
+
+    private void HandleBeginDrag(UIInventoryItem item)
+    {
+        
+    }
+
+    private void HandleItemSelection(UIInventoryItem item)
+    {
+        Debug.Log(item.name);
+        item.gameObject.SetActive(true);
+        listOfUIItems[0].Select();
     }
 
     public void Show()
     {
         gameObject.SetActive(true);
+
+        listOfUIItems[0].SetData(listOfObjects[0].lootSprite, 1);
     }
     public void Hide()
     {
