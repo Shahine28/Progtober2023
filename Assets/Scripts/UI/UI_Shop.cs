fb24bb8384a9c16782f55b6295d7082ccb603991
Shahine <shahine.benthanane@edu.devinci.fr>
2023-10-25 00:31:32 +0200

    Début de dev du shop

--- Assets/Scripts/UI/UI_Shop.cs
+++ Assets/Scripts/UI/UI_Shop.cs
@@ -8,19 +8,45 @@ using UnityEngine;
 
 public class UI_Shop : MonoBehaviour
 {
-    [SerializeField] private Transform container;
-    [SerializeField] private Transform shopItemTemplate;
+    [SerializeField] private Transform contentPanel;
+    [SerializeField] private UI_ShopItem shopItemTemplate;
     public List<ItemShop> itemSold = new List<ItemShop>();
     void Start()
     {
-        
+        ClearShop();
+        for (int i = 0; i < itemSold.Count; i++)
+        {
+            if (itemSold[i].isEquipable)
+            {
+                CreateItemButton(itemSold[i].dataItemStuff.lootSprite, 
+                    itemSold[i].dataItemStuff.lootName, itemSold[i].priceItem);
+            }
+            if (!itemSold[i].isEquipable)
+            {
+                CreateItemButton(itemSold[i].dataItemFood.lootSprite,
+                    itemSold[i].dataItemFood.lootName, itemSold[i].priceItem);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateItemButton(Sprite sprite, string itemName, int itemCost)
     {
-        
+        UI_ShopItem itemButton = Instantiate(shopItemTemplate, Vector3.zero, Quaternion.identity);
+        itemButton.imageItem.sprite = sprite;
+        itemButton.nameItem.text = itemName;
+        itemButton.prixItem.text = itemCost.ToString();
+        itemButton.transform.SetParent(contentPanel);
     }
+
+    void ClearShop()
+    {
+        for (int i = contentPanel.childCount - 1; i >= 0; i--)
+        {
+            Transform child = contentPanel.GetChild(i);
+            Destroy(child.gameObject);
+        }
+    }
+
 }
 
 [Serializable]
@@ -34,6 +60,6 @@ public struct ItemShop
     [HideIf("isEquipable")]
     [AllowNesting]
     public EdibleItemSO dataItemFood;
-    [SerializeField] int priceItem;
+    public int priceItem;
 
 }
--- Assets/Scripts/UI/UI_ShopItem.cs
+++ Assets/Scripts/UI/UI_ShopItem.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UI_ShopItem : MonoBehaviour
 {
-    public 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Image imageItem;
+    public TextMeshProUGUI nameItem;
+    public TextMeshProUGUI prixItem;
 }
